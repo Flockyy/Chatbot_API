6c98ef19e276ef40ffe939a538ac96280258d314
= <=>
2023-06-26 16:45:00 +0200

    chore: python gitignore

--- src/main.py
+++ src/main.py
@@ -10,7 +10,6 @@ app = FastAPI(
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
-<<<<<<< HEAD
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -19,7 +18,5 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-=======
->>>>>>> a9bd29b27856cf77928e24d927f7302004ca934d
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
